af6d43d30ff5abc8e8097e676a8707c744f554c1
Kelly Li <kellyli@ucsb.edu>
2024-10-10 21:47:35 -0700

    changes to test cases

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -2,6 +2,8 @@ package edu.ucsb.cs156.spring.hello;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.beans.Transient;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -23,4 +25,60 @@ public class TeamTest {
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
     
+    @Test
+    public void toString_returns_correct_string() {
+        assertEquals("Team(name=test-team, members=[])", team.toString());
+    }
+
+    @Test
+    public void sameObject() {
+        Team t = team;
+        assertEquals(true, team.equals(t));
+    }
+
+    @Test
+    public void notSameClass() {
+        String n = "no";
+        assertEquals(false, team.equals(n));
+    }
+
+    @Test
+    public void otherTT(){
+        Object o = team;
+        assertEquals(true, team.equals(o));
+    }
+
+    @Test
+    public void otherTF(){
+        Team t = new Team("test-team");  
+        t.addMember("new");
+        assertEquals(false, team.equals(t));
+    }
+
+    @Test
+    public void otherF(){
+        Team t = new Team("newteam");  
+        assertEquals(false, team.equals(t));
+    }
+
+    @Test
+    public void hashcodeTest(){
+        Team t1 = new Team();
+        t1.setName("foo");
+        t1.addMember("bar");
+        Team t2 = new Team();
+        t2.setName("foo");
+        t2.addMember("bar");
+        assertEquals(t1.hashCode(), t2.hashCode());
+    }
+
+    @Test
+    public void hashTest(){
+        // instantiate t as a Team object
+        Team t = new Team();
+        int result = t.hashCode();
+        int expectedResult = 1;
+        assertEquals(expectedResult, result);
+    }
+
 }
